69e8b0e869cd316500023c1231918087984e79c0
amagyei <kwame.na@hotmail.com>
2023-10-15 04:10:37 +0000

    My try at a crm app

--- django-tester/tester/item/views.py
+++ django-tester/tester/item/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,6 +10,52 @@ def index(request):
     
 
 def counter(request):
-    words=request.POST['words']
-    amount_of_words=len(words.split())
-    return render(request, 'counter.html', {'amount': amount_of_words})
+    
+    posts =[1,2,3,4,5,6,8,'tim','miles','morales','peter','parker']
+    return render(request, 'counter.html' ,{  'posts': posts})
+
+
+def login(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST ['password']
+
+        user= auth.authenticate(username=username, password=password)
+
+        if user  is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, "credentials invalid")
+            return redirect('login')
+
+    return render(request,'login.html')
+def register(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        password2=request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'email already registered')
+                return redirect('register')
+            elif User.objects.filter(username=username ).exists():
+                messages.info(request,'username already used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(email=email,password=password,username=username)
+                user.save()
+                return redirect  ('login')
+        else:
+            messages.info(request, "password incorrect")
+            return redirect('register')
+        
+
+    else: 
+        return render(request,'register.html')
+    
+
+def post(request,pk):
+    return render(request,'post.html', {' pk':pk} )
